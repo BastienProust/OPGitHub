184c1e2fb927a94eb7e82adacc16c79f9cd9abc7
BastienProust <71638993+BastienProust@users.noreply.github.com>
2020-09-23 15:52:34 +0200

    Update views.py

--- welcome/views.py
+++ welcome/views.py
@@ -8,4 +8,3 @@ def index(request):
 def go():
     url = 'https://console.ocp-311-intra-dev.mousquetaires.com:443/apis/build.openshift.io/v1/namespaces/dau-g0-dev/buildconfigs/python/webhooks/3f67832fcd8bbb02/generic'
     x = requests.post(url)
-    webbrowser.open('https://google.com')

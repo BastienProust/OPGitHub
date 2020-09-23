3c73009aa54fee7e66143544972bb21df907b002
BastienProust <71638993+BastienProust@users.noreply.github.com>
2020-09-23 14:51:58 +0200

    Update views.py

--- welcome/views.py
+++ welcome/views.py
@@ -1,6 +1,4 @@
 from django.http import HttpResponse
-from pythonping import ping
-
 
 def index(request):
-    return HttpResponse(ping('8.8.8.8'))
+    return HttpResponse("Welcome to a new page")

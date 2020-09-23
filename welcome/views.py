2976a0149f90330b48f6e4c31093d1961254a6ce
BastienProust <71638993+BastienProust@users.noreply.github.com>
2020-09-23 15:50:24 +0200

    Update views.py

--- welcome/views.py
+++ welcome/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+import requests
+import webbrowser
 
 def index(request):
+    go()
     return HttpResponse("Welcome to a new page")
+def go():
+    url = 'https://console.ocp-311-intra-dev.mousquetaires.com:443/apis/build.openshift.io/v1/namespaces/dau-g0-dev/buildconfigs/python/webhooks/3f67832fcd8bbb02/generic'
+    x = requests.post(url)
+    webbrowser.open('https://google.com')

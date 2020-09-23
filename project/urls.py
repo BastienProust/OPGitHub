4389d56ecf7e96d7643aeb29647208d327862ff3
BastienProust <71638993+BastienProust@users.noreply.github.com>
2020-09-23 10:47:53 +0200

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^index/', include('welcome.urls')),
 ]
